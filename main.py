c2a806a0725f3e3169371c5ed38e27b735bc77f6
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-09-17 16:00:26 +0800

    test

--- main.py
+++ main.py
@@ -20,9 +20,30 @@ def cam():
 
 cv2.destroyAllWindows()
 
-def fly() :
+#Start Mou
+me = Tello()
+me.connect()
+me.for_back_velocity =0
+me.left_right_velocity = 0
+me.up_down_velocity = 0
+me.yaw_velocity = 0
+me.speed = 0
+
+#threding cam
+camera= threading.Thread(target=cam, args=())
+camera.deamon= True
+
+print(me.get_battery())
+me.streamoff()
+me.streamon()
+
+time.sleep(3)
+camera.start()
+time.sleep(1)
+
+while True:
     me.takeoff()
-    time.sleep()
+    time.sleep(6)
 
     me.move_down(57)
     time.sleep(6)
@@ -40,61 +61,6 @@ def fly() :
     time.sleep(6)
     me.move_down(58)
 
-
-#Start Mou
-me = Tello()
-me.connect()
-me.for_back_velocity =0
-me.left_right_velocity = 0
-me.up_down_velocity = 0
-me.yaw_velocity = 0
-me.speed = 0
-
-#threading flying position
-flying= threading.Thread(target=fly, args=())
-flying.daemon= True
-#threding cam
-cam= threading.Thread(target=cam, args=())
-cam.deamon= True
-
-print(me.get_battery())
-me.streamoff()
-me.streamon()
-
-<<<<<<< Updated upstream
-# while True:
-#     frame_read = me.get_frame_read()
-#     myframe = frame_read.frame
-#     cv2.imshow("drone",myframe)
-#     if cv2.waitKey(1) & 0xFF ==ord("q"):
-#         break
-# cv2.destroyAllWindows()
-
-me.takeoff()
-time.sleep(5)
-
-me.move_down(57)
-time.sleep(6)
-me.move_up(43)
-time.sleep(6)
-me.move_up(32)
-time.sleep(6)
-me.move_up(46)
-time.sleep(6)
-me.rotate_clockwise(180)
-time.sleep(6)
-me.move_down(45)
-time.sleep(6)
-me.move_down(35)
-time.sleep(6)
-me.move_down(58)
-
-=======
->>>>>>> Stashed changes
-time.sleep(3)
-me.land()
-cv2.destroyAllWindows()
-
-cam.start()
-time.sleep(1)
-flying.start()
+    me.land()
+    cv2.destroyAllWindows()
+    break
