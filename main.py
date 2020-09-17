7748087a3287b5b5052c32a58b2abf18a6e430a1
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-09-17 18:17:18 +0800

    pushing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import cv2
 from djitellopy import Tello
 import threading
-import pyzbar.pyzbar as pyzbar
 from pyzbar import pyzbar
 import time
 import numpy as np
@@ -9,65 +8,64 @@ from csv import writer
 import datetime
 import argparse
 
+startscanning = 0
+
 def scan():
-    # construct the argument parser and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-o", "--output", type=str, default="barcode.csv",
-                    help="path to output CSV file containing barcodes")
-    args = vars(ap.parse_args())
+    global myframe
+    global startscanning
+    if startscanning == 1:
+        # construct the argument parser and parse the arguments
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-o", "--output", type=str, default="barcode.csv",
+                        help="path to output CSV file containing barcodes")
+        args = vars(ap.parse_args())
 
-    cap = cv2.VideoCapture(0)
+        # open the output CSV file for writing and initialize the set of
+        csv = open(args["output"], "w")
+        found = set()
 
-    # open the output CSV file for writing and initialize the set of
-    csv = open(args["output"], "w")
-    found = set()
+        while True:
+            cv2.imshow("drone", myframe)
 
-    while True:
-        ret, frame = cap.read()
+            barcodes = pyzbar.decode(myframe)
+            # loop barcode
+            for barcode in barcodes:
+                (x, y, w, h) = barcode.rect
+                cv2.rectangle(myframe, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                barcodeData = barcode.data.decode("utf-8")
+                barcodeType = barcode.type
+                text = "{} ({})".format(barcodeData, barcodeType)
+                cv2.putText(myframe, text, (x, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        barcodes = pyzbar.decode(frame)
-        # loop barcode
-        for barcode in barcodes:
-            (x, y, w, h) = barcode.rect
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            barcodeData = barcode.data.decode("utf-8")
-            barcodeType = barcode.type
-            text = "{} ({})".format(barcodeData, barcodeType)
-            cv2.putText(frame, text, (x, y - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                # if the barcode text is currently not in our CSV file, write
+                # the timestamp + barcode to disk and update the set
+                if barcodeData not in found:
+                    csv.write("{},{}\n".format(datetime.datetime.now(),
+                                               barcodeData))
+                    csv.flush()
+                    found.add(barcodeData)
 
-            # if the barcode text is currently not in our CSV file, write
-            # the timestamp + barcode to disk and update the set
-            if barcodeData not in found:
-                csv.write("{},{}\n".format(datetime.datetime.now(),
-                                           barcodeData))
-                csv.flush()
-                found.add(barcodeData)
+            # show the frame
+            # cv2.resizeWindow('output', 625,450)
+            cv2.imshow('output', myframe)
 
-        # show the frame
-        # cv2.resizeWindow('output', 625,450)
-        cv2.imshow('output', frame)
+            key = cv2.waitKey(8) & 0xFF
+            if key == ord("q"):
+                break
 
-        key = cv2.waitKey(8) & 0xFF
-        if key == ord("q"):
-            break
-
-    cap.release()
-    csv.close
-    cv2.destroyAllWindows()
+        csv.close
+        cv2.destroyAllWindows()
 
 def cam():
     print('start cam')
-    global outputFrame
-    global startstream
+    global myframe
+    global startscanning
     while True:
         frame_read = me.get_frame_read()
         myframe = frame_read.frame
-        cv2.imshow("drone",myframe)
-        if cv2.waitKey(1) & 0xFF ==ord("q"):
-            break
+        startscanning == 1
 
-cv2.destroyAllWindows()
 
 #Start Mou
 me = Tello()
@@ -78,13 +76,17 @@ me.up_down_velocity = 0
 me.yaw_velocity = 0
 me.speed = 0
 
-#threding cam
-camera= threading.Thread(target=cam, args=())
-camera.deamon= True
-
 print(me.get_battery())
 me.streamoff()
 me.streamon()
+
+#threding cam
+camera= threading.Thread(target=cam, args=())
+camera.deamon= True
+#threding scanning
+scanning= threading.Thread(target=scan, args=())
+scanning.deamon= True
+
 
 time.sleep(3)
 camera.start()
@@ -112,5 +114,4 @@ while True:
     time.sleep(6)
 
     me.land()
-    cv2.destroyAllWindows()
     break
