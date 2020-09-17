46a574f23a47682aeabaa5154dcc9536c6cc837e
cqp7dm2 <44236908+cqp7dm2@users.noreply.github.com>
2020-09-17 14:08:12 +0800

    damn

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import cv2
 from djitellopy import Tello
+import threading
+#import pyzbar.pyzbar as pyzbar
 import time
 import numpy as np
 from csv import writer
 import datetime
 
+def cam():
+    print('start cam')
+    global outputFrame
+    global startstream
+    while True:
+        frame_read = me.get_frame_read()
+        myframe = frame_read.frame
+        cv2.imshow("drone",myframe)
+        if cv2.waitKey(1) & 0xFF ==ord("q"):
+            break
+
+cv2.destroyAllWindows()
+
+def fly() :
+    me.takeoff()
+    time.sleep()
+
+    me.move_down(57)
+    time.sleep(6)
+    me.move_up(43)
+    time.sleep(6)
+    me.move_up(32)
+    time.sleep(6)
+    me.move_up(46)
+    time.sleep(6)
+    me.rotate_clockwise(180)
+    time.sleep(6)
+    me.move_down(45)
+    time.sleep(6)
+    me.move_down(35)
+    time.sleep(6)
+    me.move_down(58)
+
+
+#Start Mou
 me = Tello()
 me.connect()
 me.for_back_velocity =0
@@ -13,10 +50,18 @@ me.up_down_velocity = 0
 me.yaw_velocity = 0
 me.speed = 0
 
+#threading flying position
+flying= threading.Thread(target=fly, args=())
+flying.daemon= True
+#threding cam
+cam= threading.Thread(target=cam, args=())
+cam.deamon= True
+
 print(me.get_battery())
 me.streamoff()
 me.streamon()
 
+<<<<<<< Updated upstream
 # while True:
 #     frame_read = me.get_frame_read()
 #     myframe = frame_read.frame
@@ -44,6 +89,12 @@ me.move_down(35)
 time.sleep(6)
 me.move_down(58)
 
+=======
+>>>>>>> Stashed changes
 time.sleep(3)
 me.land()
 cv2.destroyAllWindows()
+
+cam.start()
+time.sleep(1)
+flying.start()
